214359886440eb9e53010738340e98b4df65771c
jzl吉志龙 <zlji@Ctrip.com>
2016-06-17 21:23:43 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,19 @@
 
 from distutils.core import setup
 
-
-def find_requirements():
-    ret = []
-    with open('requirements.pip') as f:
-        for line in f:
-            if not line.startswith('#'):
-                ret.append(line)
-    return ret
-
-
 setup(
     name='docker-make',
     description='a tool for simplifying docker image building and pushing',
-    version='1.0',
+    version='1.0.1',
     author='jizhilong',
     author_email='zhilongji@gmail.com',
     url='https://github.com/CtripCloud/docker-make',
     license='Apache',
     keywords=['docker', 'image',' build'],
     scripts=['docker-make'],
-    install_requires=find_requirements(),
+    install_requires=[
+        'PyYAML >= 3.10, < 4',
+        'docker-py >= 1.8.1, < 2',
+    ],
     classifiers=[],
     )
